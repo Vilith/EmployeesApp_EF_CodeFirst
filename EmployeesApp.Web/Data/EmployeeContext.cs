d8f87ac9bca40e4a27005ca27362149ae32bbff5
Vilith <vilith44@gmail.com>
2025-06-02 13:52:42 +0200

    changing driver.

---
+++ EmployeesApp.Web/Data/EmployeeContext.cs
@@ -0,0 +1,54 @@
+﻿using EmployeesApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EmployeesApp.Web.Data
+{
+    public class EmployeeContext(DbContextOptions<EmployeeContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Employee>().HasData
+                (
+                new Employee
+                {
+                    Id = 562,
+                    Name = "Anders Hejlsberg",
+                    Email = "Anders.Hejlsberg@outlook.com",
+                },
+                new Employee
+                {
+                    Id = 62,
+                    Name = "Kathleen Dollard",
+                    Email = "k.d@outlook.com",
+                },
+                new Employee
+                {
+                    Id = 15662,
+                    Name = "Mads Torgersen",
+                    Email = "Admin.Torgersen@outlook.com",
+                },
+                new Employee
+                {
+                    Id = 52,
+                    Name = "Scott Hanselman",
+                    Email = "s.h@outlook.com",
+                },
+                new Employee
+                {
+                    Id = 563,
+                    Name = "Jon Skeet",
+                    Email = "j.s@outlook.com",
+                }
+                );
+            
+        }
+    }
+}
+
--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -2,6 +2,8 @@ using EmployeesApp.Application.Employees.Interfaces;
 using EmployeesApp.Infrastructure.Persistance.Repositories;
 using EmployeesApp.Application.Employees.Services;
 using EmployeesApp.Web.Models;
+using EmployeesApp.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesApp.Web;
 
@@ -10,6 +12,12 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<EmployeeContext>(o =>
+            o.UseSqlServer(connString));
+
         builder.Services.AddControllersWithViews();
         builder.Services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
         builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
